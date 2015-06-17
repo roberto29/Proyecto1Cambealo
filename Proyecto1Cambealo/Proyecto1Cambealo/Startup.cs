65363b210f71fc37b7015c8aafaa57ccc6c4a87d
roberto29 <roberto_aramu@hotmail.com>
2015-06-17 13:12:23 -0600

    creacion de proyecto
    #1

---
+++ Proyecto1Cambealo/Proyecto1Cambealo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto1Cambealo.Startup))]
+namespace Proyecto1Cambealo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Proyecto1Cambealo/Proyecto1Cambealo/Users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto1Cambealo
+{
+//    Nombre Completo
+//Correo
+//Contraseña
+//Repetir Contraseña
+//Número de Teléfono
+//Edad
+
+    public class Users
+    {
+        public int Id { get; set; }
+
+        public string NombreCompleto  { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Contrasena { get; set; }
+
+        public int Telefono { get; set; }
+
+        public int Edad { get; set; }
+    }
+}
